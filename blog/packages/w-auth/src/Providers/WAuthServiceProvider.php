3ba2e8bf15dbee61c12003846c94b0ef3eb34e34
Wagner Mattei <wagner.mattei@gmail.com>
2019-09-27 13:24:40 -0300

    Iniciando criação do package

---
+++ blog/packages/w-auth/src/Providers/WAuthServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace WAuth\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class WAuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    { }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
